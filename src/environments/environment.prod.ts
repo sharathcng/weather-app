637ff27a92a54ec5ec079884673c86bf7411b262
sharath.hg <sharath.hg@teams.unvired.com>
2022-05-25 17:30:54 +0530

    image path updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIHostKeyName: 'X-RapidAPI-Key',
+  XRapidAPIHostKeyValue: 'ad6336067emsh3c3d5e800fd6ae8p148314jsn310f9088fd31',
+
 };
